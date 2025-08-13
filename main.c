c4ffdec1165d20e5348b4fb2f999dbe6cc318f0f
skye <skyeladham@gmail.com>
2025-08-13 16:59:02 -0300

    check char at pos

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 int main() {
     char foxS[] = "the quick brown fox jumps over the lazy dog";
     size_t length = strlen(foxS);
+    
 
     for(int i = 0; i<length; i++){
-        printf("times looped: %zu.\n", i);
+        //printf("times looped: %zu.\n", i);
+
+        char character = foxS[i];
+        printf("char at %zu, is: %c.\n",i,character);
     }
 
     printf("Length of string is %zu.\n", length);
