f816d00c25529feede8ed27b2437088aa207a66e
skye <skyeladham@gmail.com>
2025-08-13 17:57:06 -0300

    check for acc

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main() {
     char foxS[] = "the quick brown fox jumps over the lazy dog";
     size_t length = strlen(foxS);
-    char typed[] = "the quick brown fox jimps over the lszy dog"; // two wrong characters to test
-
+    char typed[] = "the quick brown fox jimps over the lszy dog"; // two wrong characters to test roughtly 95% acc
+    int correctChar;
     /* i dont want to type every test right now
     char typed[length + 1];
     scanf("%[^\n]s", typed);
@@ -15,18 +15,20 @@ int main() {
     for(int i = 0; i<length; i++){
         char character = foxS[i];
         char typedCharacter = typed[i];
+
         //printf("char at  pos.%zu, is: %c.\n",i,character);
         //printf("char at  pos.%zu, is: %c.\n",i,typedCharacter);
 
         if(typedCharacter==character){
 
-            printf("they are the same\n");
-        }
-        else{
-            printf("they are different\n");
+            correctChar++;
         }
     }
+    
+    float acc = ((double)correctChar/(double)length)*100;
 
+    printf("acc is: %f\n", acc);
     //printf("Length of string is %zu.\n", length);
+
     return 0;
 }
