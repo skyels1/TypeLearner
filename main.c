914e7936344137e0b85251473edb9ffd3e2c294d
skye <skyeladham@gmail.com>
2025-08-13 17:39:29 -0300

    scan for message

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 int main() {
     char foxS[] = "the quick brown fox jumps over the lazy dog";
     size_t length = strlen(foxS);
+    char typed[length + 1];
+    scanf("%[^\n]s", typed);
+    printf("%s\n", typed);
     
-
+/*
     for(int i = 0; i<length; i++){
-        //printf("times looped: %zu.\n", i);
-
         char character = foxS[i];
-        printf("char at %zu, is: %c.\n",i,character);
+        printf("char at  pos.%zu, is: %c.\n",i,character);
     }
-
-    printf("Length of string is %zu.\n", length);
+*/
+    //printf("Length of string is %zu.\n", length);
     return 0;
 }
