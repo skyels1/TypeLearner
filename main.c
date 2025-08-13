2c24d15bfb41de74f8d12c9c24e143ad52ffc5db
skye <skyeladham@gmail.com>
2025-08-13 19:38:24 -0300

    wpm still not great

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <time.h>
 
 int main() {
     char foxS[] = "the quick brown fox jumps over the lazy dog";
     size_t length = strlen(foxS);
-    char typed[] = "the quick brown fox jimps over the lszy dog"; // two wrong characters to test roughtly 95% acc
     int correctChar = 0;
     int wordCount = 0;
-    int WPM = 0;
+    time_t start, end;
 
     printf("Type what is shown below:\n\n");
     printf("%s\n\n", foxS);
 
-    //char typed[length + 1];
-    //scanf("%[^\n]s", typed);
+    time(&start);
+
+    char typed[length + 1];
+    scanf("%[^\n]", typed);
+
+    time(&end);
 
     // loop to check if the typed string is the same as what is given and for the amount of words
     for(int i = 0; i<length; i++){
@@ -30,13 +33,18 @@ int main() {
             correctChar++;
         }
     }
-    printf("word count of: %i\n", wordCount + 1);
+    wordCount++;
+    double deltaTime = difftime(end, start);
 
-    printf("\n\nWPM is: %.0f\n", WPM);
+    printf("\nYou took %.0f seconds.\n", deltaTime);
+
+    double WPM = ((wordCount * 60) / deltaTime);
+
+    printf("\nWPM is: %.0f\n", WPM);
     // calculate the acc
     float acc = ((double)correctChar/(double)length)*100;
     // print stats
-    printf("\nacc is: %.0f\n", acc);
+    printf("\nacc is: %.0f%%\n", acc);
 
     return 0;
 }
