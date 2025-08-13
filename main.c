b57210c84567fa027dcde7aec4e5bfa95463ab7e
skye <skyeladham@gmail.com>
2025-08-13 16:49:29 -0300

    added a loop for checking correct characters

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <string.h>
 
 int main() {
-    char myString[] = "the quick brown fox jumps over the lazy dog";
-    size_t length = strlen(myString);
+    char foxS[] = "the quick brown fox jumps over the lazy dog";
+    size_t length = strlen(foxS);
+
+    for(int i = 0; i<length; i++){
+        printf("times looped: %zu.\n", i);
+    }
 
     printf("Length of string is %zu.\n", length);
     return 0;
